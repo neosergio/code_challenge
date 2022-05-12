7741539ddf997279f0c7ef24d4d96f8c60df187b
Sergio Infante <rsinfante@gmail.com>
2022-05-12 10:15:12 -0500

    Add documentation to tests

--- my_app/tests/functional/test_calculate_api.py
+++ my_app/tests/functional/test_calculate_api.py
@@ -1,21 +1,41 @@
 def test_home_page_with_fixture(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
     response = test_client.get("/")
     assert response.status_code == 200
 
 
 def test_calculate_with_valid_numbers(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/calculate' endpoint is requested (GET) with list of numbers
+    THEN check that the response is valid and proper calculation
+    """
     response = test_client.get("/calculate?numbers=1,8,6,2,5,4,8,3,7")
     assert response.status_code == 200
     assert response.json["max_area"] == 49
 
 
 def test_calculate_with_invalid_numbers(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/calculate' endpoint is requested (GET) with a text
+    THEN check that the response is valid and error message is shown
+    """
     response = test_client.get("/calculate?numbers=text")
     assert response.status_code == 200
     assert response.json["error"] == "no numbers in list"
 
 
 def test_calculate_with_no_parameters(test_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/calculate' endpoint is requested (GET) with no parameter
+    THEN check that the response is valid and max_area is null / none
+    """
     response = test_client.get("/calculate")
     assert response.status_code == 200
     assert response.json["max_area"] is None
--- my_app/tests/unit/test_calculate.py
+++ my_app/tests/unit/test_calculate.py
@@ -2,6 +2,11 @@ from my_app.calculate import calculate_max_area
 
 
 def test_calculate_max_area():
+    """
+    GIVEN a function to calculate max area
+    WHEN a list of numbers (more than 1) is set as a parameter
+    THEN check the calculated max area correctly
+    """
     result_1 = calculate_max_area([1, 8, 6, 2, 5, 4, 8, 3, 7])
     result_2 = calculate_max_area([1, 1])
     assert isinstance(result_1, int)
@@ -11,6 +16,11 @@ def test_calculate_max_area():
 
 
 def test_calculate_max_area_with_invalid_list():
+    """
+    GIVEN a function to calculate max area
+    WHEN a list of invalid data is set as a parameter
+    THEN check the calculated max area is 0
+    """
     result_1 = calculate_max_area(["test"])
     result_2 = calculate_max_area([1])
     result_3 = calculate_max_area([])
